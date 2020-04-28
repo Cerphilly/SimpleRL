df836114463736a1c8b34574926c5ccf709119a5
cocel <cukie1948@gmail.com>
2020-04-28 17:11:33 +0900

    remodeling

--- Remodeling/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Tensorflow + Pytorch compatible
-tensorboard -> pytorch도 tensorboard가 된다니 amazing
-matplotlib도 할까 -> tensorboard로 먼저 하고 그 다음에 csv파일을 다운 받으면 되겠지 보류
-allow gpu and cpu setting
-save model in h5 file
-allow algorithm to run -> save -> run -> save
-'''
