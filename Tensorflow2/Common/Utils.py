530656d0c60a1dcbb455d4b231c19bb3a44efce0
cocel <gyh1948@postech.ac.kr>
2020-05-22 19:42:18 +0900

    backup

--- Tensorflow2/Common/Utils.py
+++ Tensorflow2/Common/Utils.py
@@ -1,12 +1,9 @@
-
 def copy_weight(network, target_network):
     variable1 = network.trainable_variables
     variable2 = target_network.trainable_variables
 
     for v1, v2 in zip(variable1, variable2):
         v2.assign(v1)
-
-
 
 def soft_update(network, target_network, tau):
     variable1 = network.trainable_variables
--- test.py
+++ test.py
@@ -1,220 +1,230 @@
-# Author : Nikhil Advani
-# Date : 22nd May 2017
-# Description:  This programs runs a simulation of a cart-pole system
-#				The conrol algorithm used to balance the pendulum can either be using LQR or PID
-#				Please have a look at the README and the project report.
-
+import torch
+import torch.nn as nn
+from torch.distributions import MultivariateNormal
+import gym
 import numpy as np
-import cv2, math, time
-import matplotlib.pyplot as plt
-import sys
-from control.matlab import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class Cart:
-    def __init__(self, x, mass, world_size):
-        self.x = x
-        self.y = int(0.6 * world_size)  # 0.6 was chosen for aesthetic reasons.
-        self.mass = mass
-        self.color = (0, 255, 0)
+class Memory:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+        self.is_terminals = []
+
+    def clear_memory(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprobs[:]
+        del self.rewards[:]
+        del self.is_terminals[:]
 
 
-class Pendulum:
-    def __init__(self, length, theta, ball_mass):
-        self.length = length
-        self.theta = theta
-        self.ball_mass = ball_mass
-        self.color = (0, 0, 255)
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, action_std):
+        super(ActorCritic, self).__init__()
+        # action mean range -1 to 1
+        self.actor = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 32),
+            nn.Tanh(),
+            nn.Linear(32, action_dim),
+            nn.Tanh()
+        )
+        # critic
+        self.critic = nn.Sequential(
+            nn.Linear(state_dim, 64),
+            nn.Tanh(),
+            nn.Linear(64, 32),
+            nn.Tanh(),
+            nn.Linear(32, 1)
+        )
+        self.action_var = torch.full((action_dim,), action_std * action_std).to(device)
+
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state, memory):
+        action_mean = self.actor(state)
+        cov_mat = torch.diag(self.action_var).to(device)
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+        action = dist.sample()
+        action_logprob = dist.log_prob(action)
+
+        memory.states.append(state)
+        memory.actions.append(action)
+        memory.logprobs.append(action_logprob)
+
+        return action.detach()
+
+    def evaluate(self, state, action):
+        action_mean = self.actor(state)
+
+        action_var = self.action_var.expand_as(action_mean)
+        cov_mat = torch.diag_embed(action_var).to(device)
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+
+        action_logprobs = dist.log_prob(action)
+        dist_entropy = dist.entropy()
+        state_value = self.critic(state)
+
+        return action_logprobs, torch.squeeze(state_value), dist_entropy
 
 
-def display_stuff(world_size, cart, pendulum):
-    # This function displays the pendulum and cart.
-    length_for_display = pendulum.length * 100
-    A = np.zeros((world_size, world_size, 3), np.uint8)
-    cv2.line(A, (0, int(0.6 * world_size)), (world_size, int(0.6 * world_size)), (255, 255, 255), 2)
-    cv2.rectangle(A, (int(cart.x) + 25, cart.y + 15), (int(cart.x) - 25, cart.y - 15), cart.color, -1)
-    pendulum_x_endpoint = int(cart.x - (length_for_display) * math.sin(pendulum.theta))
-    pendulum_y_endpoint = int(cart.y - (length_for_display) * math.cos(pendulum.theta))
-    cv2.line(A, (int(cart.x), cart.y), (pendulum_x_endpoint, pendulum_y_endpoint), pendulum.color, 4)
-    cv2.circle(A, (pendulum_x_endpoint, pendulum_y_endpoint), 6, (255, 255, 255), -1)
-    cv2.imshow('WindowName', A)
-    cv2.waitKey(5)
+class PPO:
+    def __init__(self, state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip):
+        self.lr = lr
+        self.betas = betas
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+
+        self.policy = ActorCritic(state_dim, action_dim, action_std).to(device)
+        self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr, betas=betas)
+
+        self.policy_old = ActorCritic(state_dim, action_dim, action_std).to(device)
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+        self.MseLoss = nn.MSELoss()
+
+    def select_action(self, state, memory):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        return self.policy_old.act(state, memory).cpu().data.numpy().flatten()
+
+    def update(self, memory):
+        # Monte Carlo estimate of rewards:
+        rewards = []
+        discounted_reward = 0
+        for reward, is_terminal in zip(reversed(memory.rewards), reversed(memory.is_terminals)):
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
+            rewards.insert(0, discounted_reward)
+
+        # Normalizing the rewards:
+        rewards = torch.tensor(rewards).to(device)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+
+        # convert list to tensor
+        old_states = torch.squeeze(torch.stack(memory.states).to(device), 1).detach()
+        old_actions = torch.squeeze(torch.stack(memory.actions).to(device), 1).detach()
+        old_logprobs = torch.squeeze(torch.stack(memory.logprobs), 1).to(device).detach()
+
+        # Optimize policy for K epochs:
+        for _ in range(self.K_epochs):
+            # Evaluating old actions and values :
+            logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
+
+            # Finding the ratio (pi_theta / pi_theta__old):
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+
+            # Finding Surrogate Loss:
+            advantages = rewards - state_values.detach()
+            surr1 = ratios * advantages
+            surr2 = torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
+            loss = -torch.min(surr1, surr2) + 0.5 * self.MseLoss(state_values, rewards) - 0.01 * dist_entropy
+
+            # take gradient step
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+
+        # Copy new weights into old policy:
+        self.policy_old.load_state_dict(self.policy.state_dict())
 
 
-def find_pid_control_input(cart, pendulum, time_delta, error, previous_error, integral, g):
-    # Using PID to find control inputs
+def main():
+    ############## Hyperparameters ##############
+    env_name = "Pendulum-v0"
+    render = False
+    solved_reward = 0  # stop training if avg_reward > solved_reward
+    log_interval = 1  # print avg reward in the interval
+    max_episodes = 10000  # max training episodes
+    max_timesteps = 200  # max timesteps in one episode
 
-    # The gains were emperically tuned
-    Kp = -150
-    Kd = -20
-    Ki = -20
+    update_timestep = 10  # update policy every n timesteps
+    action_std = 0.5  # constant std for action distribution (Multivariate Normal)
+    K_epochs = 32  # update policy for K epochs
+    eps_clip = 0.2  # clip parameter for PPO
+    gamma = 0.99  # discount factor
 
-    derivative = (error - previous_error) / time_delta
-    integral += error * time_delta
-    F = (Kp * error) + (Kd * derivative) + (Ki * integral)
-    return F, integral
+    lr = 0.0003  # parameters for Adam optimizer
+    betas = (0.9, 0.999)
 
+    random_seed = None
+    #############################################
 
-def find_lqr_control_input(cart, pendulum, time_delta, error, previous_error, theta_dot, x_dot, g):
-    # Using LQR to find control inputs
+    # creating environment
+    env = gym.make(env_name)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
 
-    # The A and B matrices are derived in the report
-    A = np.matrix([
-        [0, 1, 0, 0],
-        [0, 0, g * pendulum.ball_mass / cart.mass, 0],
-        [0, 0, 0, 1],
-        [0, 0, (cart.mass + pendulum.ball_mass) * g / (pendulum.length * cart.mass), 0]
-    ])
+    if random_seed:
+        print("Random Seed: {}".format(random_seed))
+        torch.manual_seed(random_seed)
+        env.seed(random_seed)
+        np.random.seed(random_seed)
 
-    B = np.matrix([
-        [0],
-        [1 / cart.mass],
-        [0],
-        [1 / (pendulum.length * cart.mass)]
-    ])
+    memory = Memory()
+    ppo = PPO(state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
+    print(lr, betas)
 
-    # The Q and R matrices are emperically tuned. It is described further in the report
-    Q = np.matrix([
-        [10, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 10000, 0],
-        [0, 0, 0, 100]
-    ])
+    # logging variables
+    running_reward = 0
+    avg_length = 0
+    time_step = 0
 
-    R = np.matrix([500])
+    # training loop
+    for i_episode in range(1, max_episodes + 1):
+        state = env.reset()
+        for t in range(max_timesteps):
+            time_step += 1
+            # Running policy_old:
+            action = ppo.select_action(state, memory)
+            state, reward, done, _ = env.step(action)
 
-    # The K matrix is calculated using the lqr function from the controls library
-    K, S, E = lqr(A, B, Q, R)
-    np.matrix(K)
+            # Saving reward and is_terminals:
+            memory.rewards.append(reward)
+            memory.is_terminals.append(done)
 
-    x = np.matrix([
-        [np.squeeze(np.asarray(cart.x))],
-        [np.squeeze(np.asarray(x_dot))],
-        [np.squeeze(np.asarray(pendulum.theta))],
-        [np.squeeze(np.asarray(theta_dot))]
-    ])
+            # update if its time
+            if time_step % update_timestep == 0:
+                ppo.update(memory)
+                memory.clear_memory()
+                time_step = 0
+            running_reward += reward
+            if render:
+                env.render()
+            if done:
+                break
 
-    desired = np.matrix([
-        [300],
-        [0],
-        [0],
-        [0]
-    ])
+        avg_length += t
 
-    F = -(K * (x - desired))
-    return np.squeeze(np.asarray(F))
+        # stop training if avg_reward > solved_reward
+        if running_reward > (log_interval * solved_reward):
+            print("########## Solved! ##########")
+            torch.save(ppo.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format(env_name))
+            break
 
+        # save every 500 episodes
+        if i_episode % 500 == 0:
+            torch.save(ppo.policy.state_dict(), './PPO_continuous_{}.pth'.format(env_name))
 
-def apply_control_input(cart, pendulum, F, time_delta, x_tminus2, theta_dot, theta_tminus2, previous_time_delta, g):
-    # Finding x and theta on considering the control inputs and the dynamics of the system
-    theta_double_dot = (((cart.mass + pendulum.ball_mass) * g * math.sin(pendulum.theta)) + (
-                F * math.cos(pendulum.theta)) - (pendulum.ball_mass * ((theta_dot) ** 2.0) * pendulum.length * math.sin(
-        pendulum.theta) * math.cos(pendulum.theta))) / (pendulum.length * (
-                cart.mass + (pendulum.ball_mass * (math.sin(pendulum.theta) ** 2.0))))
-    x_double_dot = ((pendulum.ball_mass * g * math.sin(pendulum.theta) * math.cos(pendulum.theta)) - (
-                pendulum.ball_mass * pendulum.length * math.sin(pendulum.theta) * (theta_dot ** 2)) + (F)) / (
-                               cart.mass + (pendulum.ball_mass * (math.sin(pendulum.theta) ** 2)))
-    cart.x += ((time_delta ** 2) * x_double_dot) + (((cart.x - x_tminus2) * time_delta) / previous_time_delta)
-    pendulum.theta += ((time_delta ** 2) * theta_double_dot) + (
-                ((pendulum.theta - theta_tminus2) * time_delta) / previous_time_delta)
+        # logging
+        if i_episode % log_interval == 0:
+            avg_length = int(avg_length / log_interval)
+            running_reward = int((running_reward / log_interval))
 
-
-def find_error(pendulum):
-    # There's a seperate function for this because of the wrap-around problem
-    # This function returns the error
-    previous_error = (pendulum.theta % (2 * math.pi)) - 0
-    if previous_error > math.pi:
-        previous_error = previous_error - (2 * math.pi)
-    return previous_error
-
-
-def plot_graphs(times, errors, theta, force, x):
-    # This function plots all the graphs
-    plt.subplot(4, 1, 1)
-    plt.plot(times, errors, '-b')
-    plt.ylabel('Error')
-    plt.xlabel('Time')
-
-    plt.subplot(4, 1, 2)
-    plt.plot(times, theta, '-b')
-    plt.ylabel('Theta')
-    plt.xlabel('Time')
-
-    plt.subplot(4, 1, 3)
-    plt.plot(times, force, '-b')
-    plt.ylabel('Force')
-    plt.xlabel('Time')
-
-    plt.subplot(4, 1, 4)
-    plt.plot(times, x, '-b')
-    plt.ylabel('X')
-    plt.xlabel('Time')
-
-    plt.show()
+            print('Episode {} \t Avg length: {} \t Avg reward: {}'.format(i_episode, avg_length, running_reward))
+            running_reward = 0
+            avg_length = 0
 
 
-def main(control_technique):
-    # Initializing mass values, g, world size, simulation time and variables required to terminate the simulation
-    mass_of_ball = 1.0
-    mass_of_cart = 5.0
-    g = 9.81
-    errors, force, theta, times, x = [], [], [], [], []
-    world_size = 1000
-    simulation_time = 35
-    previous_timestamp = time.time()
-    end_time = previous_timestamp + simulation_time
-
-    # Initializing cart and pendulum objects
-    cart = Cart(int(0.2 * world_size), mass_of_cart, world_size)
-    pendulum = Pendulum(1, 1, mass_of_ball)
-
-    # Initializing other variables needed for the simulation
-    theta_dot = 0
-    theta_tminus1 = theta_tminus2 = pendulum.theta
-    x_tminus1 = x_tminus2 = cart.x
-    previous_error = find_error(pendulum)
-    integral = 0
-    previous_time_delta = 0
-
-    # The simulation must run for the desired amount of time
-    while time.time() <= end_time:
-        current_timestamp = time.time()
-        time_delta = (current_timestamp - previous_timestamp)
-        error = find_error(pendulum)
-        if previous_time_delta != 0:  # This condition is to make sure that theta_dot is not infinity in the first step
-            theta_dot = (theta_tminus1 - theta_tminus2) / previous_time_delta
-            x_dot = (x_tminus1 - x_tminus2) / previous_time_delta
-            if control_technique == "lqr":
-                F = find_lqr_control_input(cart, pendulum, time_delta, error, previous_error, theta_dot, x_dot, g)
-            elif control_technique == "pid":
-                F, intergral = find_pid_control_input(cart, pendulum, time_delta, error, previous_error, integral, g)
-            apply_control_input(cart, pendulum, F, time_delta, x_tminus2, theta_dot, theta_tminus2, previous_time_delta,
-                                g)
-
-            # For plotting the graphs
-            force.append(F)
-            x.append(cart.x)
-            errors.append(error)
-            times.append(current_timestamp)
-            theta.append(pendulum.theta)
-
-        # Update the variables and display stuff
-        display_stuff(world_size, cart, pendulum)
-        previous_time_delta = time_delta
-        previous_timestamp = current_timestamp
-        previous_error = error
-        theta_tminus2 = theta_tminus1
-        theta_tminus1 = pendulum.theta
-        x_tminus2 = x_tminus1
-        x_tminus1 = cart.x
-
-    plot_graphs(times, errors, theta, force, x)
-
-
-if __name__ == "__main__":
-    arguments = sys.argv
-    if len(arguments) < 2:
-        print("Useage: python inverted_pendulum.py <control_technique>")
-        sys.exit()
-    control_technique = arguments[1]
-    main(control_technique)
+if __name__ == '__main__':
+    main()
