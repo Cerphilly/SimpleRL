235b0cd7c013265204044cf49c4d9a43a4125bae
cocel <gyh1948@postech.ac.kr>
2020-05-28 18:13:58 +0900

    backup

--- Tensorflow2/Algorithms/Dueling DQN.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-from Tensorflow2.Common.TFBuffer import TFBuffer
-from Tensorflow2.Common.TFSaver import TFSaver
-from Tensorflow2.Common.Utils import copy_weight
-
-
--- Tensorflow2/Trainer/DM_trainer.py
+++
@@ -1,2 +0,0 @@
-from dm_control import suite
-#shouldn't I use dmcontrol2gym for multiple environment use?
