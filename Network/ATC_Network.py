e7c3d729b6a4146b8799ef0f3fb2f8f2bb5d3831
Cerphilly <cukie1948@gmail.com>
2022-05-29 18:13:34 +0900

    remake

--- Network/ATC_Network.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-#linear global compressor g_\phi to produce a small latent code vector c_t
-class Compressor(tf.keras.Model):
-    pass
-
-#residual predictor MLP h_psi, which acts as an implicit forwrad model to advance the code p_t = h_\psi(c_t) + c_t
-class Predictor(tf.keras.Model):
-    pass
